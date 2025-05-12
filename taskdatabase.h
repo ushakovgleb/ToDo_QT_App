f2c37b3cf3b9b90dc74a4a6db1bd832be249eda9
Gleb Ushakov <101178703+ushakovgleb@users.noreply.github.com>
2025-05-12 10:28:19 +0300

    Реализация структуры задачи

--- taskdatabase.h
+++ taskdatabase.h
@@ -1,6 +1,23 @@
 #ifndef TASKDATABASE_H
 #define TASKDATABASE_H
 
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QVariant>
+#include <QDate>
+#include <QDebug>
+
+
+struct Task {
+    int id;
+    Qstring name;
+    Qstring tags;
+    Qstring category;
+    QDate deadline;
+    bool completed;
+};
+
 class TaskDatabase
 {
 public:
