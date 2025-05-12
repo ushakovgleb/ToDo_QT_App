5fca4fd305e635183f927891b1c647728c3591f6
Gleb Ushakov <101178703+ushakovgleb@users.noreply.github.com>
2025-05-12 11:01:21 +0300

    Добавление проверки правильного открытия БД

--- taskdatabase.cpp
+++ taskdatabase.cpp
@@ -1,3 +1,16 @@
 #include "taskdatabase.h"
 
-TaskDatabase::TaskDatabase() {}
+TaskDatabase::TaskDatabase() {
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("tasks.db");
+}
+
+bool TaskDatabase::open() {
+    if (!db.open()) {
+        qDebug() << "Не удалось открыть базу данных:" << db.lastError.text();
+        return false;
+    }
+
+    init();
+    return true;
+}
